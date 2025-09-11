8f18d1dfb9002ca9b698971f86cd0ba9a2de9b9d
Filipe N. Soares <fn697169@gmail.com>
2025-09-11 15:32:49 -0300

    chorus: routes

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,24 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func (c *fiber.Ctx) error {
-
+		return c.SendString("home")
 	})
+
+	app.Get("/login", func (c *fiber.Ctx) error {
+		return c.SendString("login")
+	})
+
+	app.Post("/form_upload", func (c *fiber.Ctx) error {
+		return c.SendString("form_upload")
+	})
+
+	app.Get("/image", func (c *fiber.Ctx) error {
+		return c.SendString("image")
+	})
+
+	app.Get("/list_all_participants", func (c *fiber.Ctx) error {
+		return c.SendString("list_all_participants")
+	})
+
+
 }
